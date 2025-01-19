148226c7470342a4d4552ba1133db8d5e453629d
Ajith PM <ajithpmdwaraka@gmail.com>
2025-01-19 22:05:14 +0530

    Models written

--- onlineshop/serializers.py
+++ onlineshop/serializers.py
@@ -1,1 +1,9 @@
+ from rest_framework import serializers
+ from .models import order
  
+ 
+ class OrderSerializar(serializers.ModelSerializer):
+     
+    class Meta:
+        model = order
+        fields = '__all__'
